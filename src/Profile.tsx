45e213baa7f990f21bc5266c12f7ff79ef39f020
“kuramoto_taiga” <kuramoto_taiga@plise.co.jp>
2024-08-06 20:03:32 +0900

    Init commit

---
+++ src/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return (
+    <div className="profile">
+      <h1>自己紹介ページ</h1>
+      <p>自己紹介ページです</p>
+    </div>
+  );
+};
+
+export default Profile;
